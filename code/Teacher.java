1e3623028e791ff121b6c99747e153baca05fd73
Anmol Poudyal <poudelanmol1@gmail>
2024-01-31 19:26:24 +0545

    This is java coursework

---
+++ code/Teacher.java
@@ -0,0 +1,78 @@
+package Project_CourseWork;
+
+/**
+ * This is a parent class of the program.
+ *
+ * @author (Anmol Poudyal)
+ * @version (2023/12/29)
+ */
+public class Teacher
+{
+   private int teacherId;
+   private int workingHr;
+   private String teacherName;
+   private String address;
+   private String workType;
+   private String employeeStatus;
+   //creating the constructor to inniatialize value
+   public Teacher(int teacherId,String teacherName,String address,String workType,String employeeStatus)
+   {
+       this.teacherId = teacherId;
+       this.teacherName = teacherName;
+       this.address = address;
+       this.workType = workType;
+       this.employeeStatus = employeeStatus;
+   }
+   /* creating the getter metghod for every instances that are private which could be used to access when needed in other classes */
+   public int getTeacherId()
+   {
+       return teacherId;
+   }    
+   
+   public String getTeacherName()
+   {
+       return teacherName;
+   }
+   
+   public String getAddress()
+   {
+       return address;
+   } 
+   
+   public String getWorkType()
+   {
+       return workType;
+   }
+    
+   public String getEmployeeStatus()
+    {
+        return employeeStatus;
+    }
+    
+    public int getWorkingHr()
+    {
+        return workingHr;
+    }
+
+    public void setWorkingHr(int workingHr)
+    {
+        this.workingHr = workingHr;
+    }
+    
+    public void displayInfo() //this method displays the values for the teaacher class
+    {
+        System.out.println("The allocated teacher_id is "+teacherId);
+        System.out.println("The name of the teacher_name is "+teacherName);
+        System.out.println("The teacher address is "+address);
+        System.out.println("The working type of that particular teacher is "+ workType);
+        System.out.println("The teacher is currently "+employeeStatus);
+        if (workingHr == 0)          // if the worrking hr is 0 we assume that the working hr is not set yet
+        {
+            System.out.println(" oppps!! The working hour is not assigned yet ");    
+        }
+        else           
+        {
+            System.out.println("the working hour is "+workingHr);
+        }
+    }
+}
---
+++ code/Tutor.java
@@ -0,0 +1,113 @@
+package Project_CourseWork;
+
+/**
+ * This is a child class of the program.
+ *
+ * @author (Anmol Poudyal)
+ * @version (2023/12/29)
+ */
+public class Tutor extends Teacher
+{
+    private double salary;
+    private String specialization;
+    private String academicQualifications;
+    private int performanceIndex;
+    private boolean isCertified;
+    
+    public Tutor(int teacherId,String teacherName,String address,String workType,String employeeStatus,int workingHr,double salary,
+    String specialization,String academicQualifications,int performanceIndex)
+    {
+        super(teacherId,teacherName,address,workType,employeeStatus);   //calling the super class constructor and initializing the values
+        super.setWorkingHr(workingHr);
+        this.salary=salary;
+        this.specialization=specialization;
+        this.academicQualifications=academicQualifications;
+        this.performanceIndex=performanceIndex;
+        this.isCertified=false;
+        
+    }
+    public double getSalary()   //all the get method below are used to accesss instance variable 
+    {
+        return salary;
+    }
+    
+    public String getSpecialization()
+    {
+        return specialization;
+    }
+    
+    public String getAcademicQualifications()
+    {
+        return academicQualifications;
+    }
+    
+    public int  getPerformanceIndex()
+    {
+        return  performanceIndex;
+    }
+    
+    public boolean getIsCertified()
+    {
+        return isCertified;
+    }
+    
+    public void setSalary(double Salary,int PerformanceIndex)   //this method sets the salary of the teacher according to course work critearea
+    {
+        
+        if ( PerformanceIndex>=5 && getWorkingHr()>20 )
+        {
+            if (PerformanceIndex>=5 && PerformanceIndex<=7)
+            {
+                Salary = (Salary + ((5/100f)*Salary));
+                  this.salary=Salary;
+                  this.isCertified=true;
+            }
+            else if(PerformanceIndex>=8 && PerformanceIndex<=9)
+            {
+                Salary = (Salary + ((10/100f)*Salary));
+                  this.salary=Salary;
+                  this.isCertified=true;
+            }
+            else if(PerformanceIndex==10)
+            {
+                Salary = (Salary + ((20/100f)*Salary));
+                  this.salary=Salary;
+                  this.isCertified=true;
+            }
+            else
+            {
+                System.out.println("invalid result");
+            }
+        }
+        else
+        {
+            System.out.println("your salary cannot be approved");
+        }
+        
+    }
+    public void removeTutor()   //this method removes the teacher if any of them are not certified 
+    {
+    
+         if (isCertified == false)
+         {
+             this.salary= 0;
+             this.specialization="";
+             this.academicQualifications="";
+             this.performanceIndex=0;
+             System.out.println("removed successfully......Thank u");
+             this.isCertified=false;
+         }
+    }
+    public void displayInfo() // this method displays the information
+    {
+        super.displayInfo(); 
+
+        if (isCertified)
+        {
+            System.out.println("The salary is " + salary);
+            System.out.println("Specialized in " +specialization);
+            System.out.println("Academic qualification " +academicQualifications);
+            System.out.println("teachers performance index is  " +performanceIndex);
+        }
+    }
+}
